ca6794cef4b3688237a435dcd0381f38dafca408
Khensani <129982152+khensani8@users.noreply.github.com>
2024-08-13 17:27:32 +0200

    small changes to js file

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,14 @@
 let johannesburgElement = document.querySelector("#johannesburg");
 let johannesburgDateElement = johannesburgElement.querySelector(".date");
-let johannesburgTime = moment().tz("Africa/Johannesburg");
-let johannesburgTimeElement = johannesburgElement.querySelector("#time");
+let johannesburgTimeElement = johannesburgElement.querySelector(".time");
 
-johannesburgDateElement.innerHTML = johannesburgTime.format("Do MMMM YYYY");
-johannesburgTimeElement.innerHTML = johannesburgTime.format(
-  "h:mm:ss[<small>]A[</small>]"
-);
+// Function to update the time
+function updateJohannesburgTime() {
+  let johannesburgTime = moment().tz("Africa/Johannesburg");
+  johannesburgDateElement.innerHTML = johannesburgTime.format("Do MMMM YYYY");
+  johannesburgTimeElement.innerHTML = johannesburgTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+}
+
+setInterval(updateJohannesburgTime);
